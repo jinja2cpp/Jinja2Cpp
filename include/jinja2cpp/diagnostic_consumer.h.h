2ed9f2a631fcbafea7fa7fb391876d71eb0b5b62
Flex Ferrum <flexferrum@gmail.com>
2018-04-27 15:34:04 +0000

    Add TemplateEnv and IDiagnosticConsumer prototypes

---
+++ include/jinja2cpp/diagnostic_consumer.h.h
@@ -0,0 +1,16 @@
+#ifndef JINJA2CPP_DIAGNOSTIC_CONSUMER_H
+#define JINJA2CPP_DIAGNOSTIC_CONSUMER_H
+
+#include "template.h"
+
+namespace jinja2
+{
+
+class IDiagnosticConsumer
+{
+public:
+};
+
+} // jinja2
+
+#endif // JINJA2CPP_DIAGNOSTIC_CONSUMER_H
---
+++ include/jinja2cpp/template_env.h.h
@@ -0,0 +1,16 @@
+#ifndef JINJA2CPP_TEMPLATE_ENV_H
+#define JINJA2CPP_TEMPLATE_ENV_H
+
+#include "template.h"
+
+namespace jinja2
+{
+
+class TemplateEnv
+{
+public:
+};
+
+} // jinja2
+
+#endif // JINJA2CPP_TEMPLATE_ENV_H
