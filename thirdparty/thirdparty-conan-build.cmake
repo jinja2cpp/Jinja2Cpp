e6cc00e619b553bf2963dc045e48d706b37dedf2
Flex Ferrum <flexferrum@gmail.com>
2019-05-06 23:09:25 +0300

    Fix conan package libs reference

--- thirdparty/thirdparty-conan-build.cmake
+++ thirdparty/thirdparty-conan-build.cmake
@@ -4,5 +4,5 @@
 # find_package(value-ptr-lite)
 # find_package(boost)
 
-set (JINJA2_PRIVATE_LIBS_INT boost PARENT_SCOPE)
-set (JINJA2_PUBLIC_LIBS_INT expected-lite variant-lite value-ptr-lite optional-lite PARENT_SCOPE)
+set (JINJA2_PRIVATE_LIBS_INT CONAN_PKG::boost PARENT_SCOPE)
+set (JINJA2_PUBLIC_LIBS_INT CONAN_PKG::expected-lite CONAN_PKG::variant-lite CONAN_PKG::value-ptr-lite CONAN_PKG::optional-lite PARENT_SCOPE)
