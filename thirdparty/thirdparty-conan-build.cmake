77978eb5552513d30df1eace3963474451b17d53
Flex Ferrum <flexferrum@gmail.com>
2019-05-07 13:40:37 +0300

    Adopt conan-build mode for 'cmake-find-package' generator

--- thirdparty/thirdparty-conan-build.cmake
+++ thirdparty/thirdparty-conan-build.cmake
@@ -1,8 +1,8 @@
-# find_package(expected-lite)
-# find_package(variant-lite)
-# find_package(optional-lite)
-# find_package(value-ptr-lite)
-# find_package(boost)
+find_package(expected-lite)
+find_package(variant-lite)
+find_package(optional-lite)
+find_package(value-ptr-lite)
+find_package(boost)
 
 
 set (CONAN_PKG_PREFIX )
@@ -17,5 +17,5 @@ endif ()
 
 message (STATUS "JINJA2CPP_USE_CONANPKG_PREFIX = ${JINJA2CPP_USE_CONANPKG_PREFIX}, CONAN_PKG_PREFIX = ${CONAN_PKG_PREFIX}")
 
-set (JINJA2_PRIVATE_LIBS_INT ${CONAN_PKG_PREFIX}boost)
-set (JINJA2_PUBLIC_LIBS_INT ${CONAN_PKG_PREFIX}expected-lite ${CONAN_PKG_PREFIX}variant-lite ${CONAN_PKG_PREFIX}value-ptr-lite ${CONAN_PKG_PREFIX}optional-lite)
+set (JINJA2_PRIVATE_LIBS_INT boost::boost)
+set (JINJA2_PUBLIC_LIBS_INT expected-lite::expected-lite variant-lite::variant-lite value-ptr-lite::value-ptr-lite optional-lite::optional-lite)
