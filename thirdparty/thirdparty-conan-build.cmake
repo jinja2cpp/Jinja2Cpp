c8d962e3e9471bcae36a9f9c4bc0b498d6b664f5
Flex Ferrum <flexferrum@gmail.com>
2019-08-07 22:40:57 +0300

    [skip ci] Fix conan-build dep management config

--- thirdparty/thirdparty-conan-build.cmake
+++ thirdparty/thirdparty-conan-build.cmake
@@ -3,8 +3,9 @@ message(STATUS "'conan-build' dependencies mode selected for Jinja2Cpp. All depe
 find_package(expected-lite)
 find_package(variant-lite)
 find_package(optional-lite)
+find_package(string-view-lite)
 find_package(boost)
 find_package(fmtlib)
 
 set (JINJA2_PRIVATE_LIBS_INT boost::boost fmt::fmt)
-set (JINJA2_PUBLIC_LIBS_INT expected-lite::expected-lite variant-lite::variant-lite optional-lite::optional-lite)
+set (JINJA2_PUBLIC_LIBS_INT expected-lite::expected-lite variant-lite::variant-lite optional-lite::optional-lite string-view-lite::string-view-lite)
