9db14f180da01b2d43f654c70383a5d20c313e37
Flex Ferrum <flexferrum@gmail.com>
2019-09-01 00:40:06 +0300

    [skip ci] Fix conan_build dependency management build scenario

--- thirdparty/thirdparty-conan-build.cmake
+++ thirdparty/thirdparty-conan-build.cmake
@@ -5,7 +5,22 @@ find_package(variant-lite)
 find_package(optional-lite)
 find_package(string-view-lite)
 find_package(boost)
-find_package(fmtlib)
+find_package(fmt)
 
-set (JINJA2_PRIVATE_LIBS_INT boost::boost fmt::fmt)
+update_submodule(robin-hood-hashing)
+add_library(rh_lib INTERFACE)
+target_include_directories(rh_lib
+        INTERFACE
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/robin-hood-hashing/src/include>
+        $<INSTALL_INTERFACE:include>)
+
+set (JINJA2_PRIVATE_LIBS_INT boost::boost fmt::fmt rh_lib)
 set (JINJA2_PUBLIC_LIBS_INT expected-lite::expected-lite variant-lite::variant-lite optional-lite::optional-lite string-view-lite::string-view-lite)
+
+install(TARGETS rh_lib
+        EXPORT InstallTargets
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}/static
+        )
+
