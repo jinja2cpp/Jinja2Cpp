666208a59f2da987f0eedbf911d00af987aa7b58
Flex Ferrum <flexferrum@gmail.com>
2019-11-17 12:51:15 +0200

    Distinguish boost conan.io package name on Windows and Linux

--- thirdparty/thirdparty-conan-build.cmake
+++ thirdparty/thirdparty-conan-build.cmake
@@ -4,9 +4,18 @@ find_package(expected-lite)
 find_package(variant-lite)
 find_package(optional-lite)
 find_package(string-view-lite)
-find_package(Boost)
+
+# Temporary workaround for conan.io issue with boost package naming
+if (WIN32 OR WIN64)
+    find_package(boost)
+    set(CONAN_BOOST_PACKAGE_NAME boost::boost)
+else ()
+    find_package(Boost)
+    set(CONAN_BOOST_PACKAGE_NAME Boost::Boost)
+endif ()
+
 find_package(fmt)
 find_package(rapidjson)
 
-set(JINJA2_PRIVATE_LIBS_INT Boost::Boost fmt::fmt rapidjson::rapidjson)
+set(JINJA2_PRIVATE_LIBS_INT CONAN_BOOST_PACKAGE_NAME fmt::fmt rapidjson::rapidjson)
 set(JINJA2_PUBLIC_LIBS_INT expected-lite::expected-lite variant-lite::variant-lite optional-lite::optional-lite string-view-lite::string-view-lite)
