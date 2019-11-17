e0a7db075bfbbd9fd324f5f431bf82ff51a0a32e
Flex Ferrum <flexferrum@gmail.com>
2019-11-17 13:13:52 +0200

    Updage boost conan.io package name

--- thirdparty/thirdparty-conan-build.cmake
+++ thirdparty/thirdparty-conan-build.cmake
@@ -5,17 +5,10 @@ find_package(variant-lite)
 find_package(optional-lite)
 find_package(string-view-lite)
 
-# Temporary workaround for conan.io issue with boost package naming
-if (WIN32 OR WIN64)
-    find_package(boost)
-    set(CONAN_BOOST_PACKAGE_NAME boost::boost)
-else ()
-    find_package(Boost)
-    set(CONAN_BOOST_PACKAGE_NAME Boost::Boost)
-endif ()
-
+find_package(Boost)
+set(CONAN_BOOST_PACKAGE_NAME Boost::Boost)
 find_package(fmt)
 find_package(rapidjson)
 
-set(JINJA2_PRIVATE_LIBS_INT CONAN_BOOST_PACKAGE_NAME fmt::fmt rapidjson::rapidjson)
+set(JINJA2_PRIVATE_LIBS_INT ${CONAN_BOOST_PACKAGE_NAME} fmt::fmt rapidjson::rapidjson)
 set(JINJA2_PUBLIC_LIBS_INT expected-lite::expected-lite variant-lite::variant-lite optional-lite::optional-lite string-view-lite::string-view-lite)
