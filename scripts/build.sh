3cb231a3d1fd2f173abc067b6b5e8c5d7654d750
logerror <logerror@xy.hfut.edu.cn>
2021-08-12 21:27:15 +0800

    simplify the shell script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,10 @@ set -ex
 
 export BUILD_TARGET="all"
 export CMAKE_OPTS="-DCMAKE_VERBOSE_MAKEFILE=OFF"
-if [[ "${COMPILER}" != "" ]]; then export CXX=${COMPILER}; fi
-if [[ "${BUILD_CONFIG}" == "" ]]; then export BUILD_CONFIG="Release"; fi
-if [[ "${COLLECT_COVERAGE}" != "" ]]; then export BUILD_CONFIG="Debug" && export CMAKE_OPTS="${CMAKE_OPTS} -DJINJA2CPP_WITH_COVERAGE=ON"; fi
-if [[ "${SANITIZE_BUILD}" != "" ]]; then export BUILD_CONFIG="RelWithDebInfo" && export CMAKE_OPTS="${CMAKE_OPTS} -DJINJA2CPP_WITH_SANITIZERS=${SANITIZE_BUILD}"; fi
+[[ "${COMPILER}" != "" ]] && export CXX=${COMPILER}
+[[ "${BUILD_CONFIG}" == "" ]] && export BUILD_CONFIG="Release"
+[[ "${COLLECT_COVERAGE}" != "" ]] && export BUILD_CONFIG="Debug" && export CMAKE_OPTS="${CMAKE_OPTS} -DJINJA2CPP_WITH_COVERAGE=ON"
+[[ "${SANITIZE_BUILD}" != "" ]] && export BUILD_CONFIG="RelWithDebInfo" && export CMAKE_OPTS="${CMAKE_OPTS} -DJINJA2CPP_WITH_SANITIZERS=${SANITIZE_BUILD}"
 $CXX --version
 mkdir -p build && cd build
 cmake $CMAKE_OPTS -DCMAKE_BUILD_TYPE=$BUILD_CONFIG -DCMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS -DJINJA2CPP_DEPS_MODE=internal $EXTRA_FLAGS .. && cmake --build . --config $BUILD_CONFIG --target all -- -j4
