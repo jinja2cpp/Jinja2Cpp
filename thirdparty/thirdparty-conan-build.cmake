bb8d1635dd0f646e14539731b9c051b1b7845e64
Ruslan Morozov <ruslan.y.morozov@gmail.com>
2021-08-26 00:59:03 +0300

    make jinja2cpp conan-friendly again

--- thirdparty/thirdparty-conan-build.cmake
+++ thirdparty/thirdparty-conan-build.cmake
@@ -1,14 +1,15 @@
 message(STATUS "'conan-build' dependencies mode selected for Jinja2Cpp. All dependencies are taken as a conan packages")
 
-find_package(expected-lite)
-find_package(variant-lite)
-find_package(optional-lite)
-find_package(string-view-lite)
+find_package(expected-lite REQUIRED)
+find_package(variant-lite REQUIRED)
+find_package(optional-lite REQUIRED)
+find_package(string-view-lite REQUIRED)
+find_package(nlohmann_json REQUIRED)
 
 find_package(Boost)
 set(CONAN_BOOST_PACKAGE_NAME Boost::Boost)
 find_package(fmt)
-find_package(rapidjson)
+find_package(RapidJSON)
 
-set(JINJA2_PRIVATE_LIBS_INT ${CONAN_BOOST_PACKAGE_NAME} fmt::fmt rapidjson::rapidjson)
-set(JINJA2_PUBLIC_LIBS_INT expected-lite::expected-lite variant-lite::variant-lite optional-lite::optional-lite string-view-lite::string-view-lite)
+set(JINJA2_PRIVATE_LIBS_INT ${CONAN_BOOST_PACKAGE_NAME} fmt::fmt RapidJSON::RapidJSON nlohmann_json::nlohmann_json)
+set(JINJA2_PUBLIC_LIBS_INT nonstd::expected-lite nonstd::variant-lite nonstd::optional-lite nonstd::string-view-lite)
