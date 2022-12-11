4f0f30ea2ad65d59d4fc9d790dd5131a1ed9d259
Ruslan Morozov <ruslan.y.morozov@gmail.com>
2022-12-11 19:54:52 +0300

    remove comments

--- thirdparty/thirdparty-internal.cmake
+++ thirdparty/thirdparty-internal.cmake
@@ -2,7 +2,6 @@ message(STATUS "'internal' dependencies mode selected for Jinja2Cpp. All depende
 
 include (./thirdparty/internal_deps.cmake)
 
-#update_submodule(boost)
 set(BOOST_ENABLE_CMAKE ON)
 list(APPEND BOOST_INCLUDE_LIBRARIES
     algorithm
@@ -23,55 +22,6 @@ FetchContent_Declare(
   PATCH_COMMAND git apply --ignore-whitespace "${CMAKE_CURRENT_LIST_DIR}/../cmake/patches/0001-fix-skip-install-rules.patch" || true
 )
 FetchContent_MakeAvailable(Boost)
-#find_package(Boost REQUIRED COMPONENTS     algorithm
-#    assert
-#    atomic
-#    filesystem
-#    lexical_cast
-#    optional
-#    variant
-#    json)
-
-#find_package(boost_algorithm REQUIRED)
-#find_package(boost_atomic REQUIRED)
-#find_package(boost_filesystem  REQUIRED)
-#find_package(boost_lexical_cast  REQUIRED)
-#find_package(boost_optional  REQUIRED)
-#find_package(boost_variant  REQUIRED)
-#set(BOOST_INCLUDE_LIBRARIES ${BOOST_INCLUDE_LIBRARIES} CACHE INTERNAL "")
-#add_subdirectory(thirdparty/boost)
-
-#include(FetchContent)
-#
-## BOOST
-#
-#set(BOOST_REQD_SUBMODULES
-#    "tools/cmake;"
-#    "libs/assert;libs/exception;libs/throw_exception;libs/static_assert;"
-#    "libs/bind;libs/function_types;libs/function;"
-#    "libs/chrono;libs/date_time;"
-#    "libs/concept_check;"
-#    "libs/config;libs/container;libs/container_hash;"
-#    "libs/iterator;libs/utility;libs/type_traits;libs/algorithm;;libs/conversion;libs/numeric/conversion;libs/regex;libs/unordered;libs/tokenizer;libs/move;libs/ratio;libs/lexical_cast;"
-#    "libs/tuple;libs/variant2;libs/typeof;libs/detail;libs/array;libs/type_index;libs/range;libs/optional;libs/smart_ptr;libs/integer;libs/rational;"
-#    "libs/intrusive;libs/io;"
-#    "libs/core;libs/align;libs/predef;libs/preprocessor;libs/system;libs/winapi;libs/atomic;"
-#    "libs/mpl;libs/fusion;libs/mp11;"
-#    "libs/thread"
-#)
-#
-#Set(FETCHCONTENT_QUIET FALSE)
-#FetchContent_Declare(
-#    boost
-#    GIT_REPOSITORY "https://github.com/boostorg/boost.git"
-#    GIT_TAG master
-#    GIT_SUBMODULES ${BOOST_REQD_SUBMODULES}
-#    GIT_PROGRESS TRUE
-#    CONFIGURE_COMMAND ""  # tell CMake it's not a cmake project
-#)
-#
-#FetchContent_MakeAvailable(boost)
-
 
 if(NOT MSVC)
     # Enable -Werror and -Wall on jinja2cpp target, ignoring warning errors from thirdparty libs
@@ -84,6 +34,7 @@ if(NOT MSVC)
         target_compile_options(boost_assert INTERFACE -Wno-error=parentheses)
     endif()
     if(COMPILER_HAS_WNO_ERROR_DEPRECATED_DECLARATIONS_FLAG)
+        target_compile_options(boost_unordered PRIVATE -Wno-error=deprecated-declarations)
         target_compile_options(boost_filesystem PRIVATE -Wno-error=deprecated-declarations)
     endif()
     if(COMPILER_HAS_WNO_ERROR_MAYBE_UNINITIALIZED_FLAG)
