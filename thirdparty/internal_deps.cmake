f63f427a78496cb7dc2cdfab47e9b32089c6f1ca
Ruslan Morozov <ruslan.y.morozov@gmail.com>
2022-12-17 20:25:05 +0300

    fix #233
    install preserving directory structure
    
    unfortunately to fix it using cmake-native mechanics it required to bump cmake version to 3.23.0

--- thirdparty/internal_deps.cmake
+++ thirdparty/internal_deps.cmake
@@ -3,20 +3,16 @@ add_subdirectory(thirdparty/nonstd/expected-lite EXCLUDE_FROM_ALL)
 
 update_submodule(nonstd/variant-lite)
 add_subdirectory(thirdparty/nonstd/variant-lite EXCLUDE_FROM_ALL)
-add_library(variant-lite ALIAS variant-lite)
 
 update_submodule(nonstd/optional-lite)
 add_subdirectory(thirdparty/nonstd/optional-lite EXCLUDE_FROM_ALL)
-add_library(optional-lite ALIAS optional-lite)
 
 update_submodule(nonstd/string-view-lite)
 add_subdirectory(thirdparty/nonstd/string-view-lite EXCLUDE_FROM_ALL)
-add_library(string-view-lite ALIAS string-view-lite)
 
 update_submodule(fmtlib)
 set (FMT_INSTALL ON CACHE BOOL "" FORCE)
 add_subdirectory(thirdparty/fmtlib EXCLUDE_FROM_ALL)
-add_library(fmt ALIAS fmt-header-only)
 
 update_submodule(json/rapid)
 set (RAPIDJSON_BUILD_DOC OFF CACHE BOOL "" FORCE)
