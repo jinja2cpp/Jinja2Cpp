11ebd1e3dec209c9d972e077c967f6ac58e3350b
Ruslan Morozov <ruslan.y.morozov@gmail.com>
2023-11-17 20:42:03 +0300

    rework deps population using CMake fetch_content module

--- thirdparty/thirdparty-external-boost.cmake
+++ thirdparty/thirdparty-external-boost.cmake
@@ -1,4 +1,5 @@
-message(STATUS "'external-boost' dependencies mode selected for Jinja2Cpp. All dependencies are used as submodules except of boost")
+message(STATUS "'external-boost' dependencies mode selected for Jinja2Cpp. All
+dependencies are built from source pulled from github except of boost")
 
 include (./thirdparty/internal_deps.cmake)
 include (./thirdparty/external_boost_deps.cmake)
